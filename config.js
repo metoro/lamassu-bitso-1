cad31b3f91ef17e09b519eafb2178dd42135028e
Neal Conner <neal.conner@lamassu.is>
2014-09-29 23:12:32 -0400

    Updated to Bitso

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 
 var _ = require('lodash');
 
-exports.NAME = 'Bitstamp';
+exports.NAME = 'Bitso';
 exports.SUPPORTED_MODULES = ['ticker', 'trader'];
-exports.API_ENDPOINT = 'https://www.bitstamp.net/api/';
+exports.API_ENDPOINT = 'https://api.bitso.com/v2/';
 
 
 exports.SATOSHI_FACTOR = 1e8;
